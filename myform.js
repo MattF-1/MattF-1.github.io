dae5db197e6ce8fcff07807507d8c1f8bd5269ae
MattF-1 <matthew1.christopher@torontomu.ca>
2024-10-30 15:16:59 -0400

    j

---
+++ myform.js
@@ -0,0 +1,17 @@
+window.onload=function(){
+    var user = document.getElementById("username");
+    console.log(user.value); // the user input value.
+    console.log(user.type); // the type of the form element.
+    }
+<input type="text" id="myinput" oninput="inputHandler()">
+<p id="emptyParagraph">Dynamically change on webpage input without
+prompt</p>
+
+function inputHandler()
+{
+var myvalue = document.getElementById("myinput").value;
+document.getElementById("emptyParagraph").innerHTML = "Your Input:" + myvalue;
+}
+localStorage.setItem("username", "marijn"); // store the items
+console.log(localStorage.getItem("username"));// → marijn
+localStorage.removeItem("username"); // remove from the localstorage
